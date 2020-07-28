52180b0be83c53a5418a0984e87f012c198b8bf5
Igor Karotki <igorkarotki@gmail.com>
2020-07-28 13:53:24 +0200

    28.07

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
   (function () {
-   'use strict';
+    'use strict';
 
-   let btnScrollDown = document.querySelector('#scroll_down');
+    let btnScrollDown = document.querySelector('#scroll_down');
 
-   function scrollDown() {
-    let windowCoords = document.documentElement.clientHeight;
-    (function scroll() {
-     if (window.pageYOffset < windowCoords) {
-      window.scrollBy(0, 10);
-      setTimeout(scroll, 0);
-     }
-     if (window.pageYOffset > windowCoords) {
-      window.scrollTo(0, windowCoords);
-     }
-    })();
-   }
+    function scrollDown() {
+      let windowCoords = document.documentElement.clientHeight;
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 10);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
 
-   btnScrollDown.addEventListener('click', scrollDown);
+    btnScrollDown.addEventListener('click', scrollDown);
   }());
